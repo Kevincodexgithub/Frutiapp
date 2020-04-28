596656c5be1f7d52c900e0ff57e1d855a9691537
kevincodexgithub <kevintauro95@gmail.com>
2020-04-27 22:05:32 -0500

    Agregacion de sumas dinamicas y numeros aleatorios

--- app/src/main/java/com/example/frutiapp/Main2Activity_Nivel1.java
+++ app/src/main/java/com/example/frutiapp/Main2Activity_Nivel1.java
@@ -2,6 +2,7 @@ package com.example.frutiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -16,7 +17,7 @@ public class Main2Activity_Nivel1 extends AppCompatActivity {
     private EditText et_respuestas;
     private MediaPlayer mp,mp_great,mp_bad;
 
-    int score, numAleatorio_uno,numAleatorio_dos,resultados, vidas = 3;
+    int score, numAleatorio_uno,numAleatorio_dos,resultado, vidas = 3;
     String nombre_jugador, string_score, string_vidas;
 
     String numero[] = {"cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
@@ -45,8 +46,46 @@ public class Main2Activity_Nivel1 extends AppCompatActivity {
         mp.start();
         mp.setLooping(true);
 
+        //Inicializados los sonidos secundarios
         mp_great = MediaPlayer.create(this,R.raw.wonderful);
         mp_bad = MediaPlayer.create(this,R.raw.bad);
 
+        NumAleatorio();
+    }
+
+    public void NumAleatorio(){
+        if(score <= 9){
+            numAleatorio_uno = (int) (Math.random() * 10);
+            numAleatorio_dos = (int) (Math.random() * 10);
+
+            resultado = numAleatorio_uno + numAleatorio_dos;
+
+            if(resultado <= 10){
+                for(int i = 0; i < numero.length;i++){
+                    int id = getResources().getIdentifier(numero[i],"drawable",getPackageName());
+                    if(numAleatorio_uno == i){
+                        iv_Auno.setImageResource(id);
+                    }
+                    if(numAleatorio_dos == i){
+                        iv_Ados.setImageResource(id);
+                    }
+                }
+            }else{
+                NumAleatorio();
+            }
+
+        }else{
+            Intent intent = new Intent(this,Main2Activity_Nivel2.class);
+            string_score = String.valueOf(score);
+            string_vidas = String.valueOf(vidas);
+            intent.putExtra("jugador",nombre_jugador);
+            intent.putExtra("score",string_score);
+            intent.putExtra("vidas",string_vidas);
+
+            startActivity(intent);
+            finish();
+            mp.stop();
+            mp.release();
+        }
     }
 }
